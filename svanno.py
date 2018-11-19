24ac48404eb77806743552e9a27c3eaf253f47db
Teng Gao <gaot@luna.cbio.private>
2018-11-19 18:06:47 -0500

    include gene class

--- svanno.py
+++ svanno.py
@@ -34,7 +34,17 @@ def sort_gene(genes):
     else:
         return sorted_genes[0]
     
-def annotate(bedpe):
+def annotate(bedpe, hide = True):
+    
+    # oncogenes and tumor suppressors
+    onco = []
+    tsg = []
+    import requests
+    for gene in requests.get("http://oncokb.org/api/v1/genes").json():
+        if gene["oncogene"]:
+            onco.append(gene["hugoSymbol"])
+        if gene["tsg"]:
+            tsg.append(gene["hugoSymbol"])
     
     bedpe = bedpe.copy()
     
@@ -53,6 +63,9 @@ def annotate(bedpe):
 
     bedpe['gene1'] = bedpe['gene1'].apply(lambda x: x if x else 'Intergenic')
     bedpe['gene2'] = bedpe['gene2'].apply(lambda x: x if x else 'Intergenic')
+    
+    bedpe['gene_class1'] = bedpe['gene1'].apply(lambda x: '(ONC)' if x in onco else '(TSG)' if x in tsg else '')
+    bedpe['gene_class2'] = bedpe['gene2'].apply(lambda x: '(ONC)' if x in onco else '(TSG)' if x in tsg else '')
 
     bedpe['exon1'] = bedpe['exon1'].apply(lambda x: 'exon' + x if x else '')
     bedpe['exon2'] = bedpe['exon2'].apply(lambda x: 'exon' + x if x else '')
@@ -63,12 +76,13 @@ def annotate(bedpe):
     bedpe['utr1'] = bedpe['utr1'].str.replace('five_prime_', '5p').str.replace('three_prime_', '3p')
     bedpe['utr2'] = bedpe['utr2'].str.replace('five_prime_', '5p').str.replace('three_prime_', '3p')
 
-    bedpe['annot'] = bedpe.apply(lambda row: '%s%s>>%s%s' % (row['gene1'] + ':' if row['gene1'] != 'Intergenic' and (row['exon1'] or row['intron1'] or row['utr1']) else row['gene1'],
+    bedpe['annot'] = bedpe.apply(lambda row: '%s%s>>%s%s' % (row['gene1'] + row['gene_class1'] + ':' if row['gene1'] != 'Intergenic' and (row['exon1'] or row['intron1'] or row['utr1']) else row['gene1'],
                                                             row['utr1'] if row['utr1'] else row['exon1'] if row['exon1'] else row['intron1'],
-                                                            row['gene2'] + ':' if row['gene2'] != 'Intergenic' and (row['exon2'] or row['intron2'] or row['utr2']) else row['gene2'],
+                                                            row['gene2'] + row['gene_class2'] + ':' if row['gene2'] != 'Intergenic' and (row['exon2'] or row['intron2'] or row['utr2']) else row['gene2'],
                                                             row['utr2'] if row['utr2'] else row['exon2'] if row['exon2'] else row['intron2']), axis = 1)
-
-    bedpe = bedpe[['chr1', 'start1', 'end1', 'chr2', 'start2', 'end2', 'name', 'score', 'strand1', 'strand2', 'type', 'annot']]
+    
+    if hide:
+        bedpe = bedpe[['chr1', 'start1', 'end1', 'chr2', 'start2', 'end2', 'name', 'score', 'strand1', 'strand2', 'type', 'annot']]
     
     return bedpe
 
